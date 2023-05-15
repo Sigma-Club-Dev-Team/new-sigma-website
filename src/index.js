314164d11fa362df1cf94926351eebaf632d7fbb
oyerohabib <oyerohabib@yahoo.com>
2023-05-15 22:02:47 +0100

    installed chakra icons and added test header component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    purple: "#8F19E7",
+    gold: "#FEB804",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
