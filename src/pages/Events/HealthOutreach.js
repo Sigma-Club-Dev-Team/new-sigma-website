065a3f8bac6bb3c0e7d2f396fb39262c004d41e4
Olasupo Tomiwa <97839385+Olasupotomiwa@users.noreply.github.com>
2023-06-22 01:13:08 +0100

    Update HealthOutreach.js

--- src/pages/Events/HealthOutreach.js
+++ src/pages/Events/HealthOutreach.js
@@ -1,5 +1,5 @@
 import { Container } from "@chakra-ui/react";
-import QuizImage from "../../assets/pic3.jpg";
+import health from "../../assets/health.jpg";
 import { Flex } from "@chakra-ui/react";
 import Video from "../../components/Video";
 import { Text, Box, Image } from "@chakra-ui/react";
@@ -13,7 +13,7 @@ const Health = () => {
             mt={6}
             borderRadius="lg"
             width={{ base: "100%", sm: "100%" }}
-            src={QuizImage}
+            src={health}
             alt="presentation of prize"
           />
         </Box>
@@ -30,32 +30,28 @@ const Health = () => {
           <br />
 
           <Text fontSize="16px" lineHeight="normal">
-            Over the years now, Sigma Club has inducted several important
-            personalities into the Sigma Honorary Roll of Honor. The club
-            introduces these important personalities, who are of outstanding
-            personalities and have positively impacted the society, into the
-            club through the Sigma Guest Luncheon. It is a classy but educative
-            gathering of first class personalities of the country. Due to the
-            kind of event it is, attendance is only by invitation. It will
-            interest you to know that the most recent inductee is the Obi of
-            Onitsha, His Royal Majesty, Alfred Nnaemeka Achebe, CFR.
+              As part of the activities of the Club for this Sigma year, we organized the SIGMA HEALTH OUTREACH.
+The health outreach was aimed at promoting good health practices among the student body. It provided an opportunity for students to get free medical checkups, health screenings, scaling and polishing and access to health education resources.
             <br />
             <br />
             <p>
-              Among other recipients of this high honour include: Late Alhaji
-              Dr. Ado Abdullahi Bayero (CFR), Late Dr. Taslim Elias, QC, (GCON,
-              former president of the World Court), Late Dr. Alex Ekwueme (GCON,
-              former Vice President of the Federal Republic of Nigeria), Dr.
-              Christopher Kolade (CON, Nigeria’s former High Commissioner to the
-              United Kingdom), Otunba Olasubomi Balogun (CON, founder FCMB
-              Group), Professor Isaac Adewole (FAS, Nigeria’s immediate past
-              Minister for Health), Professor Babatunde Gabriel Ogunmola (FAS,
-              FAMedS(emeritus), Past President Academy of Science and the
-              Chancellor, Lead City University, Ibadan), Chief Afe Babalola
-              (SAN, CON), Chief Felix Ohiwerei (OFR, former Chairman of Nigerian
-              Breweries), His Royal Highness Alhaji Mohammadu Sanusi II (CON)
-              among others.
+          Our objectives for the Health Outreach event were clear. We aimed to offer scaling and polishing services to enhance oral hygiene; provide free HIV and syphilis screening to promote sexual health awareness; conduct mental health assistance and conduct vital signs checks to identify potential health concerns and offer guidance.
             </p>
+               <p>
+            We worked alongside Unibadan Association of Dental Students. Their expertise in oral healthcare was instrumental in offering scaling and polishing services to our participants. They also offered trainings on healthy oral health hygiene.
+Another of our esteemed partners was ASIDO, an organization specializing in mental health support. Recognizing the importance of mental well-being, we aimed to provide holistic care to our participants, addressing both their physical and emotional needs.
+            </p>
+               <br />
+         
+                <p>
+              Additionally, we partnered with iCARE, an organization focused on HIV screening and education. Their invaluable support enabled us to provide free and confidential HIV screening to all participants. 
+
+The impact of the Health Outreach event cannot be understated. Over 300 students within the university community, benefited from the free services provided. The positive feedback and engagement we received from the community served as a testament to the importance of such initiatives in addressing the health needs of our student population.
+               </p>
+               <br />
+               <p>
+              We are inspired by the impact we have made and the lives we have touched through this Health Outreach. Moving forward, we will continue to dedicate ourselves to giving back to the community.
+               </p>
           </Text>
         </Box>
       </Flex>
