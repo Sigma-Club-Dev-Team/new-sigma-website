5d8e3e6a281b5acddca9683d2020d2085601d7a1
oyerohabib <oyerohabib@yahoo.com>
2023-06-30 17:42:45 +0100

    fixed link in top storeies view all

--- src/pages/Home/TopStories.tsx
+++ src/pages/Home/TopStories.tsx
@@ -10,8 +10,9 @@ import {
   Container,
   Flex,
 } from "@chakra-ui/react";
+import { Link as RRLink } from "react-router-dom";
 import { Article } from "../../types";
-import {ArticleArray} from '../../constants/articles';
+import { ArticleArray } from "../../constants/articles";
 
 type BlogAuthorProps = {
   blog: Article;
@@ -33,62 +34,62 @@ const Blog = () => {
       <Heading as="h2">Top Stories in Our Blogs</Heading>
       {ArticleArray.slice(0, 3).map((article) => {
         return (
-         <Box
-          marginTop={{ base: "1", sm: "5" }}
-          display="flex"
-          flexDirection={{ base: "column", md: "row" }}
-          justifyContent="space-between"
-        >
           <Box
+            marginTop={{ base: "1", sm: "5" }}
             display="flex"
-            flex="1"
-            marginRight="8"
-            gap="4"
-            marginBottom="4"
-            position="relative"
-            width={{ base: "100%", sm: "100%" }}
-            alignItems="center"
-            flexDirection={{ base: "column", sm: "row" }}
+            flexDirection={{ base: "column", md: "row" }}
+            justifyContent="space-between"
           >
-            <Image
-              borderRadius="lg"
-              src={article.image}
-              alt="some good alt text"
-              objectFit="contain"
-            />
+            <Box
+              display="flex"
+              flex="1"
+              marginRight="8"
+              gap="4"
+              marginBottom="4"
+              position="relative"
+              width={{ base: "100%", sm: "100%" }}
+              alignItems="center"
+              flexDirection={{ base: "column", sm: "row" }}
+            >
+              <Image
+                borderRadius="lg"
+                src={article.image}
+                alt="some good alt text"
+                objectFit="contain"
+              />
+            </Box>
+            <Box
+              display="flex"
+              flex="1"
+              flexDirection="column"
+              justifyContent="center"
+              marginTop={{ base: "3", sm: "0" }}
+            >
+              <Heading marginTop="1">
+                <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+                  {article.title}
+                </Link>
+              </Heading>
+              <Text as="p" marginTop="2" color={"gray.700"} fontSize="lg">
+                {article.abridgedContent}
+              </Text>
+              <BlogAuthor blog={article} />
+            </Box>
           </Box>
-          <Box
-            display="flex"
-            flex="1"
-            flexDirection="column"
-            justifyContent="center"
-            marginTop={{ base: "3", sm: "0" }}
-          >
-            <Heading marginTop="1">
-              <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
-                {article.title}
-              </Link>
-            </Heading>
-            <Text as="p" marginTop="2" color={"gray.700"} fontSize="lg">
-              {article.abridgedContent}
-            </Text>
-            <BlogAuthor
-              blog={article}
-            />
-          </Box>
-        </Box>
-      
-      )})}
+        );
+      })}
       <Flex justifyContent={"center"} alignItems={"center"} mt={10}>
-        <Button
-          bg={"brand.purple"}
-          _hover={{ bg: "purple" }}
-          color={"white"}
-          size="lg"
-          fontWeight={"bold"}
-        >
-          View All
-        </Button>
+        <RRLink to={"/blog"}>
+          <Button
+            bg={"brand.purple"}
+            _hover={{ bg: "purple" }}
+            color={"white"}
+            size="lg"
+            fontWeight={"bold"}
+          >
+            View All
+          </Button>
+        </RRLink>
       </Flex>
     </Container>
   );
