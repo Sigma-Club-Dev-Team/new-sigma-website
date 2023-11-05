e2e79922dbb296ba40416bea2de613f60886f117
Korede <adeyanju.akorede1@gmail.com>
2023-11-05 09:38:26 +0100

    feat: basic 2024 quiz home page

---
+++ src/pages/Events/Quiz/Quiz2024.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { Button, Container } from "@chakra-ui/react";
+import { SecSchoolQuizGroupJPG } from "../../../constants/image_assets";
+import { Flex } from "@chakra-ui/react";
+import { Text, Box, Image } from "@chakra-ui/react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Header from "components/Header";
+import { FiChevronLeft, FiArrowRight } from "react-icons/fi";
+import Footer from "components/Footer";
+
+const Quiz = () => {
+  return (
+    <div>
+      <Header />
+
+      <Container maxW="7xl" className="animate-bottom">
+        <Flex justify={"space-between"} marginTop={"32px"}>
+          <BackButton />
+          <Text
+            fontWeight={600}
+            fontSize={"20px"}
+            lineHeight={"30px"}
+            color={"brand.purple"}
+            textDecoration={"underline"}
+            as={"a"}
+            _hover={{cursor: "pointer"}}
+          >
+            View Competition Stages
+          </Text>
+        </Flex>
+        <Text
+          mt={15}
+          display="block"
+          fontSize={"24px"}
+          lineHeight={"36px"}
+          fontWeight={600}
+          color={"#171717"}
+        >
+          2024 Roseline Etuokwu Quiz Competition Competition
+        </Text>
+        <Flex direction={{ base: "column" }}>
+          <Box color="white">
+            <Image
+              mt={"18px"}
+              borderRadius="15px"
+              width={{ base: "100%", sm: "100%" }}
+              src={SecSchoolQuizGroupJPG}
+              alt="presentation of prize"
+            />
+          </Box>
+          <Box py={{ lg: "8", md: "4", sm: "4" }}>
+            <Text fontSize="16px" lineHeight="24px" textAlign="justify">
+              The is also known as the Roseline Etuokwu Sigma Secondary School
+              Quiz Competition is one of the philanthropic activities of the
+              club to bring the club closer to the grassroots. It is the club’s
+              believe that the secondary school students would grow to become
+              University students. As such, introducing the club to them right
+              from their secondary school days would guide them in manners to
+              act and way of life to live as a student of higher institutions.
+              During this competition, moral and educational knowledge are
+              displayed and students are being trained in manners to become
+              whole human and stand “for all that is pure”. This we believe
+              would keep the students on the right track before entering the
+              university.
+              <br />
+              <br />
+              <p>
+                {" "}
+                From the inception of the competition, over 1,300 secondary
+                schools across the 6 States of the SouthWest Region of Nigeria
+                have participated and witnessed the participation of over 2,600
+                students in this competition. Apart from the participation of
+                these students and schools, gift items are also won by different
+                schools, students, and teachers for their participation.
+              </p>
+              <br />
+              <br />
+              <p>
+                {" "}
+                <Text as="i" fontWeight={600}>
+                  Prices to be won
+                  <br />
+                  Overall winner - N5,000,000; Overall 1st Runner Up -
+                  N3,000,000;
+                  <br />
+                  Overall 2nd Runner up - N2,000,000 State Champions - N150,000;
+                  <br />
+                  Regional Champions - N500,000 Including various Electronic
+                  appliances, notebooks, textbooks and gadgets to students.
+                </Text>
+              </p>
+            </Text>
+          </Box>
+        </Flex>
+
+        <Button
+          display={{ base: "inline-flex" }}
+          fontSize={"18px"}
+          lineHeight={"27px"}
+          fontWeight={500}
+          color={"white"}
+          bg={"brand.purple"}
+          gap={"8px"}
+          alignItems={"center"}
+          padding={"15px "}
+          _hover={{ bg: "brand.purple" }}
+          as={"a"}
+          href="https://forms.gle/vRhYyVyGZXjbEATi6"
+          target="_blank"
+          onClick={() => {}}
+        >
+          <Text>Register Your School</Text>
+          <FiArrowRight />
+        </Button>
+      </Container>
+
+      <Footer />
+    </div>
+  );
+};
+
+const BackButton = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <Button
+      leftIcon={<FiChevronLeft color="#333333" />}
+      padding={"0"}
+      margin={"0"}
+      backgroundColor={"transparent"}
+      _hover={{bg: "transparent"}}
+      onClick={() => {
+        if (location.key !== "default") {
+          navigate(-1);
+        }
+        navigate("/");
+      }}
+    >
+      <Text fontSize={"16px"} lineHeight={"24px"} color={"#1C1C1CB2"}>
+        Back
+      </Text>
+    </Button>
+  );
+};
+
+export default Quiz;
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      purple: "#8F19E7",
+      gold: "#FEB804",
+      text_black: "#1C1C1C",
+    },
+  },
+});
+
+
+
+export default theme;
