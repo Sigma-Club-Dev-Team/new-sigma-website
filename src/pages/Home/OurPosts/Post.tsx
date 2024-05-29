9f9068fcedfbaaf5f0db4628aa82307aff916cf9
devFemzy <adesokanemmanuelfemzy2018@gmail.com>
2024-05-29 03:15:00 +0100

    removed sass codes

--- src/pages/Home/OurPosts/Post.tsx
+++ src/pages/Home/OurPosts/Post.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import styles from "./ourPosts.module.scss"
 import { CoverImage } from 'components/myImage'
-import TextBtn from 'components/Buttons/TextBtn'
 import { ChevronsRight } from 'lucide-react'
-import { Text } from '@chakra-ui/react'
+import { Box, Button, Icon, Text } from '@chakra-ui/react'
 
-function Post() {
+
+export default function Post() {
   return (
-    <div className={styles.post}>
-        <div className={styles.image}>
+    <Box
+      width={{ base: '100%', md: '22rem', lg: '29.875rem' }}
+      minWidth={{ base: '100%', md: '22rem', lg: '29rem' }}
+      borderRadius=".5rem"
+      boxShadow="7px 7px 20px 0px #00000040"
+      overflow="hidden"
+      display="flex"
+      flexDirection="column"
+    >
+      <Box position="relative" height={{ base: '14rem', md: '17rem' }}>
         <CoverImage src='./assets/images/jpgs/health.jpg' alt='Post Image' position='center' />
-        </div>
-        <div className={styles.details}>
-        <Text noOfLines={2}>UCJ- Sigma Club quiz competition enters final stages by Akanni Oluwasegun.</Text>
-        <div className={styles.action}>
-            <TextBtn fontSize='1rem' fontWeight='400' text='View' frontArrow frontIcon={<ChevronsRight size={'1rem'} />} />
-        </div>
-        </div>
-    </div>
-  )
+      </Box>
+      <Box p="1rem" display="flex" flexDirection="column" gap=".75rem">
+        <Text noOfLines={2} fontSize="1.125rem" lineHeight="1.6875rem" fontWeight="600">UCJ- Sigma Club quiz competition enters final stages by Akanni Oluwasegun.</Text>
+        <Box>
+          <Button variant="link" rightIcon={<Icon as={ChevronsRight} w={4} h={4} />} fontSize="1rem" fontWeight="400">
+            View
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
-
-export default Post
--- src/pages/Home/OurPosts/index.tsx
+++ src/pages/Home/OurPosts/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import styles from "./ourPosts.module.scss"
-import PryBtn from 'components/Buttons/PryBtn'
-import Post from './Post'
+import React from 'react';
+import { Box, Flex, Heading, Text, SimpleGrid, Button, Icon } from '@chakra-ui/react';
+import { ChevronsRight } from 'lucide-react';
+import { CoverImage } from 'components/myImage';
+import Post from './Post';
 
-function OurPosts() {
-  const posts = [1, 2, 3, 4, 5, 6]
+export default function OurPosts() {
+  const posts = [1, 2, 3, 4, 5, 6];
   return (
-    <div className={styles.ourPostSection}>
-      <div className={styles.sectionHead}>
-        <h3>Our Posts</h3>
-        <p>View our posts and progress on some of our events and distinguished members.</p>
-      </div>
+    <Box p={{ base: '2rem 1rem 0', md: '2rem 2rem 0', lg: '2rem 6rem 0' }} display="flex" flexDirection="column" gap="1.5rem">
+      <Flex flexDirection="column" gap="1rem">
+        <Heading as="h3" fontSize="2.5rem" lineHeight="2.5rem" fontWeight="600">Our Posts</Heading>
+        <Text fontSize="1rem" fontWeight="400">View our posts and progress on some of our events and distinguished members.</Text>
+      </Flex>
 
-      <div className={styles.posts}>
+      <Flex
+        overflowX="auto"
+        pb="2rem"
+        gap="3rem"
+        css={{
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+        }}>
         {posts.map((post, index) => (
-          <Post />
+          <Post key={index} />
         ))}
-      </div>
-      <div className={styles.actions}>
-        <div></div>
-        <PryBtn text='VIEW ALL' pry />
-      </div>
-    </div>
-  )
+      </Flex>
+      
+      <Flex justifyContent="space-between" alignItems="center">
+        <Box />
+        <Button colorScheme="blue">VIEW ALL</Button>
+      </Flex>
+    </Box>
+  );
 }
-
-export default OurPosts
