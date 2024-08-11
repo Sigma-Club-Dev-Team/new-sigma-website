3d96908ebea57d579c5315d484f6ef25863ab36c
Olasupo Tomiwa <tomiwaolasupo@gmail.com>
2024-08-11 11:59:46 +0100

    updated upcoming event

--- src/pages/Home/AdmissionInterviewCard.tsx
+++ src/pages/Home/AdmissionInterviewCard.tsx
@@ -1,18 +1,35 @@
-import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Link, Text, Image } from "@chakra-ui/react";
 import CTAButton from "components/CTAButton";
 import React from "react";
+import { Walk } from "../../constants/image_assets";
 
 const AdmissionInterviewCard = () => {
   return (
     <Box
       width={"100%"}
       shadow={"10px 10px 20px 5px #00000040"}
-      padding={"75px 50px"}
-      display={"none"}
+      padding={"75px 30px"}
+      display={{ base: "block", md: "flex" }} // Adjust display for responsiveness
+      flexDirection={{ base: "column", md: "row" }} // Stack on mobile, side by side on desktop
+      gap={{ base: "20px", md: "50px" }} // Add spacing between columns
+      mx={"auto"}
+      alignItems={"center"} // Center content vertically
+      justifyContent={"center"} // Center content horizontally
+      
     >
-      <Flex flexDirection={"column"} color={"#1C1C1C"} fontWeight={600}>
+      <Flex maxW={{base: '100%', md: '75%'}}>
+
+      
+      <Flex
+        flexDirection={"column"}
+        color={"#1C1C1C"}
+        fontWeight={600}
+        flex={1}
+        alignItems={{ base: "center", md: "flex-start" }} // Center text on mobile, align left on desktop
+        textAlign={{ base: "center", md: "left" }} // Center text on mobile, align left on desktop
+      >
         <Text fontSize={"40px"} lineHeight={"60px"}>
-          2023 Sigma Admission Interview
+          Sigma Club presents Walk Against Drug Abuse💊
         </Text>
 
         <Text
@@ -22,34 +39,49 @@ const AdmissionInterviewCard = () => {
           color={"#1C1C1CB2"}
           marginTop={"20px"}
         >
-          This is to inform all male undergraduate students of the
-          The University of Ibadan that Sigma Club admission is currently ongoing.{" "}
-          <br /> <br /> Kindly fill in your application now.
+          As we mark the international youth day, it is important to stand
+          against one of the greatest vices of the youth; Drug Abuse.
         </Text>
 
-        <Flex
-          justify={"space-between"}
-          marginTop={"40px"}
-          flexDirection={{ lg: "row", base: "column" }}
-          gap={"8px"}
+        <Text fontSize={"16px"} lineHeight="24px" py={2}>
+          Date: Monday, August 12th, 2024
+          <br /> ⏰Time: 10 AM
+          <br /> 📍Start Point: SUB Car Park, UI.
+        </Text>
+
+        <Text
+          fontWeight={500}
+          fontSize={"18px"}
+          lineHeight={"27px"}
+          color={"#1C1C1CB2"}
+          marginTop={"10px"}
         >
-          <Flex
-            flexDirection={"column"}
-            gap={"25px"}
-            fontSize={"16px"}
-            lineHeight="24px"
-          >
-            <Flex gap={"100px"}>
-              <Text>Deadline: 20th November, 2023.</Text>
-            </Flex>
-          </Flex>
-          <Link
-            href="https://docs.google.com/forms/d/e/1FAIpQLScBMrttVAebCDwPZkyLZSQIXlZh58eh9ISFikIu8Y2tOkMwOA/viewform?usp=sharing"
-            isExternal
-          >
-            <CTAButton>REGISTER NOW</CTAButton>
-          </Link>
-        </Flex>
+          To be part of a walk, join the group
+        </Text>
+
+        <Link
+          href="https://bit.ly/SigWADA"
+          isExternal
+          mt={{ base: "20px" }} // Push button to the bottom on desktop
+        >
+          <CTAButton>Join Now</CTAButton>
+        </Link>
+      </Flex>
+
+      <Flex
+        flexDirection={"column"}
+        justify={"center"} // Center image vertically within its flexbox
+        alignItems={"center"} // Center image horizontally within its flexbox
+        mt={{ base: "20px", md: "0" }} // Adjust margin top for mobile
+        gap={"8px"}
+        flex={1}
+      >
+        <Image
+          src={Walk}
+          borderRadius="lg"
+          width={{ sm: "100%", md: "100%" }}
+        />
+      </Flex>
       </Flex>
     </Box>
   );
